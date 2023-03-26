3c856e523407a7789e2566ba90b7cc8bd94e540c
Vaidik <mail.vvedant@gmail.com>
2023-03-26 14:16:12 +0530

    No Change

--- calculator.py
+++ calculator.py
@@ -7,24 +7,37 @@ def add(n_1,n_2):
     return n_1 + n_2
 
 
+# Dev Goyal
+def mul(a,b):
+    return a*b
+
+=======
+def Sub(a,b):           #Vaidik _ 
+    return a - b
+
+
 num_1=int(input("Enter No1:- "))
 num_2=int(input("Enter No2:- "))
+
 print("Please Chose +,-,*,/ below or E for exit")
+
 operator=input()
 match operator:
     case ("+"):
         result=add(num_1,num_2)
         print(result)
 #Change The Case Below According To the Above Case
-    case ("+"):
-        result=add(num_1,num_2)
+    case ("-"):
+        result= Sub(num_1,num_2)
         print(result)
-    case ("+"):
-        result=add(num_1,num_2)
+    case ("*"):
+        result=mul(num_1,num_2)
         print(result)
     case ("+"):
         result=add(num_1,num_2)
         print(result)
     
 
+
+    
 
