d287a429c527e5aa88d56f239a0b786c0a80dd1b
Vaidik <mail.vvedant@gmail.com>
2023-03-26 11:42:01 +0530

    Vaidik & Subtract FUnc is added

--- calculator.py
+++ calculator.py
@@ -6,18 +6,22 @@ Also add Name Initial In comment above the code'''
 def add(n_1,n_2):
     return n_1 + n_2
 
+def Sub(a,b):           #Vaidik _ 
+    return a - b
 
 num_1=int(input("Enter No1:- "))
 num_2=int(input("Enter No2:- "))
+
 print("Please Chose +,-,*,/ below or E for exit")
+
 operator=input()
 match operator:
     case ("+"):
         result=add(num_1,num_2)
         print(result)
 #Change The Case Below According To the Above Case
-    case ("+"):
-        result=add(num_1,num_2)
+    case ("-"):
+        result= Sub(num_1,num_2)
         print(result)
     case ("+"):
         result=add(num_1,num_2)
